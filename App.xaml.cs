acf3987d33f3a1a4acc7f8ec865a3ade8b15a421
userist123 <nitumarius@yahoo.com>
2026-08-10 22:40:17 +0300

    Fix: foloseste corect proprietatea IsActivated pentru verificarea licentei

--- App.xaml.cs
+++ App.xaml.cs
@@ -25,28 +25,23 @@ public partial class App : Application
 
         var services = new ServiceCollection();
 
-        // Servicii Core (Utilitare)
         services.AddSingleton<AuditLogService>();
         services.AddSingleton<KnowledgeBaseService>();
         services.AddSingleton<PluginManagerService>();
         services.AddSingleton<LicenseService>();
 
-        // Motoarele din Infrastructure
         services.AddSingleton<IEventParser, EvtxParser>();
         services.AddSingleton<IAnalysisEngine, AnalysisEngine>();
         services.AddSingleton<IRegistryParser, RegistryParser>();
 
-        // Componentele MVVM și ferestrele din UI
         services.AddTransient<MainViewModel>();
         services.AddTransient<MainWindow>();
         services.AddTransient<ActivationWindow>();
 
         ServiceProvider = services.BuildServiceProvider();
 
-        // Verificăm licența ÎNAINTE de a afișa fereastra principală.
-        // Dacă nu este activată sau a expirat, blocăm accesul la aplicație.
         var licenseService = ServiceProvider.GetRequiredService<LicenseService>();
-        if (!licenseService.IsActivated())
+        if (!licenseService.IsActivated)
         {
             var activationWindow = ServiceProvider.GetRequiredService<ActivationWindow>();
             bool? activated = activationWindow.ShowDialog();
@@ -58,7 +53,6 @@ public partial class App : Application
             }
         }
 
-        // Afișăm fereastra principală
         var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
         mainWindow.Show();
     }

5bc00b4a6ed80433d1cf5a274c41f11165afc935
userist123 <nitumarius@yahoo.com>
2026-08-11 18:56:37 +0300

    Fix perf: AddRange foloseste notificare Add incrementala in loc de Reset

--- Services/RangeObservableCollection.cs
+++ Services/RangeObservableCollection.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
@@ -5,9 +7,11 @@ namespace LogAnalyzer.UI.Services;
 
 /// <summary>
 /// ObservableCollection care permite adăugarea în bloc a mai multor elemente
-/// generând o singură notificare CollectionChanged (Reset), în loc de N notificări
-/// individuale. Reduce drastic timpul de populare a grid-urilor WPF pentru
-/// volume mari de evenimente (zeci de mii de rânduri din fișiere EVTX).
+/// generând o singură notificare CollectionChanged de tip Add (incrementală),
+/// în loc de N notificări individuale sau de un Reset costisitor. Permite
+/// ICollectionView (cu Filter/Sort atașat) să insereze doar elementele noi,
+/// fără să reevalueze întreaga colecție la fiecare lot, ceea ce elimină
+/// saccadarea la volume mari de evenimente (zeci de mii de rânduri EVTX).
 /// </summary>
 public class RangeObservableCollection<T> : ObservableCollection<T>
 {
@@ -17,10 +21,15 @@ public class RangeObservableCollection<T> : ObservableCollection<T>
     {
         if (items == null) return;
 
+        var newItems = items as IList<T> ?? new List<T>(items);
+        if (newItems.Count == 0) return;
+
+        int startIndex = Count;
+
         _suppressNotification = true;
         try
         {
-            foreach (var item in items)
+            foreach (var item in newItems)
             {
                 Items.Add(item);
             }
@@ -32,7 +41,9 @@ public class RangeObservableCollection<T> : ObservableCollection<T>
 
         OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs(nameof(Count)));
         OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("Item[]"));
-        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+
+        var changedItems = newItems as IList ?? new List<T>(newItems);
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, changedItems, startIndex));
     }
 
     protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)

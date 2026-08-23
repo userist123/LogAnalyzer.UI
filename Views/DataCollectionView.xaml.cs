3803499f0dc2c03ce3fe2b734d033553300754f2
Marius <Marius@MARIUS-PC>
2026-08-23 13:51:48 +0300

    feat(ui): refine forensic triage collector view and modal inspection dialogs

---
+++ Views/DataCollectionView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace LogAnalyzer.UI.Views
+{
+    public partial class DataCollectionView : UserControl
+    {
+        public DataCollectionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
